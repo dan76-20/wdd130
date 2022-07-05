0ab552f7e340a4211975cbfd55d180ce09319ed0
dan76-20 <mar18109@byui.edu>
2022-07-05 16:45:57 -0600

    worked on second page for persoanl site. updated some html and css

--- goldengamers/scripts.js
+++ goldengamers/scripts.js
@@ -2,3 +2,11 @@ function join() {
     location.href="join.html";
 }
 document.querySelector('#mybutton').addEventListener("click", join);
+
+function thanks() {
+    let firstname = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let phone = document.getElementById('phone').value;
+    let email = document.getElementById('email').value;
+    alert(`Thank you ` + firstname + `. We look forward to meeting you.`)
+}
